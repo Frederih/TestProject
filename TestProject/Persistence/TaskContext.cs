5a6a6332d875434d315f5b0d39df9eb30ed7765d
Владосослав <@VLAD>
2024-02-14 13:48:21 +0200

    Refactoring

--- TestProject/Persistence/TaskContext.cs
+++ TestProject/Persistence/TaskContext.cs
@@ -5,8 +5,6 @@ namespace TestProject.Persistence
     public class TaskContext : DbContext
     {
         public DbSet<Client> Clients { get; set; }
-        public DbSet<Experiment> Experiments { get; set; }
-        public DbSet<ExperimentResult> ExperimentResults { get; set; }
         public TaskContext(DbContextOptions<TaskContext> options) : base(options)
         {
                 Database.EnsureCreated();
