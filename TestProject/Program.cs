e97d2e10b454a793d9dffcc536c883ee4b438d05
Владосослав <@VLAD>
2024-02-14 13:50:11 +0200

    Refactoring

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<TaskContext>(options => options.UseSqlServer(connection));
-builder.Services.AddTransient<ExperimentService>();
+builder.Services.AddTransient<ClientService>();
 
 builder.Services.AddControllers();
 
